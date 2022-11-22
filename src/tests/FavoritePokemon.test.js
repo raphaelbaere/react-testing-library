48936324f5194b0748ca8816dd4ad30e5167d1af
raphaelbaere <raphaelbaere@id.uff.br>
2022-11-22 15:57:25 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Renderiza o Favorites e...', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    const { history } = renderWithRouter(<FavoritePokemon />);
+    act(() => {
+      history.push('./favorites');
+    });
+    const notFoundFavorite = screen.getByText(/No favorite Pokémon found/);
+    expect(notFoundFavorite).toBeInTheDocument();
+  });
+});
